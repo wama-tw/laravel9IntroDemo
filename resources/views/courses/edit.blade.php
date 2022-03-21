f9f79fc77da9654111475da4e29f2191c8d7fee4
wama <36957874+april1026@users.noreply.github.com>
2022-03-21 19:04:49 +0800

    feat: Add old data to the edit page of courses

--- resources/views/courses/edit.blade.php
+++ resources/views/courses/edit.blade.php
@@ -8,9 +8,9 @@
 
     <form action="{{ route('course.update', ['course' => $course->id]) }}" method="POST">
         @csrf
-        課名：<input type="text" name="title"><br>
-        學分：<input type="text" name="credit"><br>
-        說明：<input type="text" name="intro"><br>
+        課名：<input type="text" name="title" value="{{ $course->title }}"><br>
+        學分：<input type="text" name="credit" value="{{ $course->credit }}"><br>
+        說明：<input type="text" name="intro" value="{{ $course->intro }}"><br>
 
         <input type="submit" value="確認送出">
     </form>
